0a0e1ca9096edc94763ddb40b86c76b4e23d1fb8
Gintautas <gintautasrepecka@gmail.com>
2018-06-05 14:56:15 +0300

    special fo Zana

---
+++ Figura.java
@@ -0,0 +1,14 @@
+package baltic.talents.lesson.answers.day4;
+
+public abstract class Figura implements Plotas, Labas {
+	
+	public String getSpalva() {
+		return "Spalva";
+	}
+
+	@Override
+	public void metodas() {
+		Plotas.super.metodas();
+	}
+	
+}
